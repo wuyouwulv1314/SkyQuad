92138147678fd1937b75782ed178153e65ed8eb5
张宇翔 <zz593141477@gmail.com>
2014-07-11 11:40:22 +0800

    SysTick has been controlled by CPAL

--- Src/stm32f10x_it.c
+++ Src/stm32f10x_it.c
@@ -127,15 +127,6 @@ void PendSV_Handler(void)
 {
 }
 
-/**
-  * @brief  This function handles SysTick Handler.
-  * @param  None
-  * @retval None
-  */
-void SysTick_Handler(void)
-{
-}
-
 /******************************************************************************/
 /*                 STM32F10x Peripherals Interrupt Handlers                   */
 /*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
